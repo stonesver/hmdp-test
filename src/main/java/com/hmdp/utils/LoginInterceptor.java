517118cb2805675d558a10144e5802e0b5ef77ee
李昊岩 <1486315284@qq.com>
2024-01-25 14:07:04 +0800

    redis实现登录流程

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+    //只负责拦截，不负责处理
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(UserHolder.getUser()==null){
+            //拦截
+            System.out.println("拦截");
+            response.setStatus(401);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
